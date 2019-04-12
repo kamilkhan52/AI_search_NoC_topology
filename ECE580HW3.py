e4220569f367e5df7d4c1f422b8b156c6db9027a
kamil <kamilkhan52@outlook.com>
2019-04-12 17:14:22 -0600

    Created a mesh

--- ECE580HW3.py
+++ ECE580HW3.py
@@ -1,7 +1,25 @@
-import csv
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 traffic_data = np.loadtxt('traffic_uniform.csv', dtype=float, delimiter=',')
 
 task_placement = np.arange(64)
-print(len(traffic_data))
+
+grid = nx.grid_2d_graph(8, 8)
+pos = dict(zip(grid, grid))
+
+nx.set_edge_attributes(grid, 1, 'length')
+
+grid.remove_edge((0, 0), (0, 1))
+grid.add_edge((0, 0), (6, 7))
+
+link_placement = nx.adjacency_matrix(grid).todense()
+
+nx.draw(grid, pos, with_labels=False)
+print(nx.nodes(grid))
+plt.show()
+
+print(link_placement)
+print(task_placement)
+
